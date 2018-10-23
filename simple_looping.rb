de54b3c5788d656b8aa6fd074c52520107a24213
SANJAY K SAI <shachsan@hotmail.com>
2018-10-23 15:28:35 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,28 +2,28 @@
 
 def loop_iterator(number_of_times)
   # Code your solution here using the "loop" keyword to puts out the below phrase
-  # This and all the other methods take an argument of an integer 
+  # This and all the other methods take an argument of an integer
   # The integer is the number of times the loops should puts out the phrase
-  # How can we make sure the loop breaks once it has puts out the phrase the 
+  # How can we make sure the loop breaks once it has puts out the phrase the
   #   correct number of times?
-  # Maybe we should keep count of the number of times we've puts out the 
+  # Maybe we should keep count of the number of times we've puts out the
   #  phrase and break when the counter hits the appropriate number...
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter =1
-  loop do 
+  loop do
     puts phrase
     counter+=1
     break if counter>number_of_times
-  end 
+  end
 end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times.times do 
+  number_of_times.times do
     puts phrase
-  end 
-  
+  end
+
 end
 
 def while_iterator(number_of_times)
@@ -34,8 +34,8 @@ def while_iterator(number_of_times)
   while i < number_of_times
     puts phrase
     i+=1
-  end 
-  
+  end
+
 end
 
 def until_iterator(number_of_times)
@@ -46,18 +46,17 @@ def until_iterator(number_of_times)
   until counter>number_of_times
     puts phrase
     counter+=1
-  end 
+  end
 
 end
 
 def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  
+
   phrase = "Welcome to Flatiron School's Web Development Course!"
   for each_time in (1..number_of_times)
     puts phrase
   end
-  
-end
 
+end
